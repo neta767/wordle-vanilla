8e92ccd1b07529ca680d04c3ed0d2c09afaf87ff
Netaly Klemann <netalyk@mail.tau.ac.il>
2022-11-27 23:05:43 +0200

    part 2

---
+++ src/application.js
@@ -0,0 +1,38 @@
+const tiles = () => document.querySelectorAll('.tile');
+const buttons = () => document.querySelectorAll('.key');
+class Game {
+    constructor(i = 0) {
+        this.index = i;
+    }
+    try(char) {
+        tiles().item(this.index).innerHTML = char;
+        this.index += 1;
+    }
+}
+// function isCharacterALetter(char) {
+//     return (/[a-zA-Z]/).test(char)
+// }
+document.addEventListener("DOMContentLoaded", () => {
+    let game = new Game();
+    buttons().forEach(btn => {
+        btn.addEventListener('pointerdown', () => {
+            game.try(btn.id);
+        });
+    });
+    document.addEventListener('keydown', event => {
+        buttons().forEach(btn => {
+            if (btn.id.toUpperCase() == event.key.toUpperCase()) {
+                game.try(btn.id);
+                btn.classList.add('key-board-down');
+            }
+        });
+    });
+    document.addEventListener('keyup', event => {
+        buttons().forEach(btn => {
+            if (btn.id.toUpperCase() == event.key.toUpperCase()) {
+                btn.classList.remove('key-board-down');
+            }
+        });
+    });
+});
+//# sourceMappingURL=application.js.map
